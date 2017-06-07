c492ebcbcc3a2ae378f8ba3a153cc79baa2940ec
dsvgit <dsv.mail@yandex.ru>
2017-06-08 01:31:39 +0300

    Initial commit

---
+++ Accounting/Accounting/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Accounting.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult GetProduct()
+        {
+            return Ok(new { test = "test data" });
+        }
+    }
+}
